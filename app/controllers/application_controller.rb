50c4f235caf1b904fd31e5203074e1bdf80c2b47
hmacht <32553134+hmacht@users.noreply.github.com>
2025-03-27 14:49:58 -0400

    removed broser version requirement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: { safari: 16, firefox: 110, ie: 9 }
 
   def check_admin
     unless current_user.admin?
